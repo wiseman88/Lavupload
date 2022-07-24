74ec73ae3326dddd0daea1b621e6ccd5f6c0805f
wiseman88 <wiseadam02@gmail.com>
2022-07-24 20:34:11 +0200

    filepond dependecies for vue added

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-
+let message = "Hello world"
 </script>
 
 <template>
-    <p>
-        Lorem ipsum dolor sit amet.
-    </p>
+    <div class="mx-auto mt-24">
+        <h1 class="text-3xl font-bold text-center">
+            Image uploader
+        </h1>
+    </div>
 </template>
 
 <style scoped>
