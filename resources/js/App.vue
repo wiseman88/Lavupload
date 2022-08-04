9ecbf54d599f9f3eea1e5ca12de60d7a58a9fd34
wiseman88 <wiseadam02@gmail.com>
2022-08-04 19:02:56 +0200

    filepond logic added

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,12 +1,36 @@
 <script setup>
-let message = "Hello world"
+// Import FilePond
+import vueFilePond from 'vue-filepond';
+import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
+
+// Import styles
+import 'filepond/dist/filepond.min.css';
+import { onMounted, ref } from 'vue';
+
+// Create FilePond component
+const FilePond = vueFilePond(FilePondPluginFileValidateType);
+
+const pond = ref(null);
+
+function filePondInitialized() {
+    console.log('Filepond is ready');
+    console.log('File is: ', pond.value);
+}
 </script>
 
 <template>
-    <div class="mx-auto mt-24">
-        <h1 class="text-3xl font-bold text-center">
+    <div class="max-w-lg mx-auto mt-24">
+        <h1 class="text-3xl font-bold text-center mb-6">
             Image uploader
         </h1>
+        <file-pond
+            name="image"
+            ref="pond"
+            label-idle="Click to choose image, or drag here..."
+            server="/upload"
+            accepted-file-types="image/*"
+            @init="filePondInitialized"
+        />
     </div>
 </template>
 
