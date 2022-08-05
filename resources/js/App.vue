416f32966018cbfc019feadccdeb3fb3a49fd0c0
wiseman88 <wiseadam02@gmail.com>
2022-08-05 22:10:45 +0200

    csrf_token handled in app.vue

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,11 +1,28 @@
 <script setup>
 // Import FilePond
-import vueFilePond from 'vue-filepond';
+import vueFilePond, { setOptions } from 'vue-filepond';
 import FilePondPluginFileValidateType from 'filepond-plugin-file-validate-type'
 
 // Import styles
 import 'filepond/dist/filepond.min.css';
 import { onMounted, ref } from 'vue';
+
+const token = document.querySelector('meta[name="csrf-token"]').content;
+
+onMounted(() => {
+    setOptions({
+        server: {
+            process: {
+                url: './upload',
+                headers: {
+                    'X-CSRF-TOKEN': token
+                }
+            }
+        }
+    });
+})
+
+console.log(token)
 
 // Create FilePond component
 const FilePond = vueFilePond(FilePondPluginFileValidateType);
@@ -23,17 +40,10 @@ function filePondInitialized() {
         <h1 class="text-3xl font-bold text-center mb-6">
             Image uploader
         </h1>
-        <file-pond
-            name="image"
-            ref="pond"
-            label-idle="Click to choose image, or drag here..."
-            server="/upload"
-            accepted-file-types="image/*"
-            @init="filePondInitialized"
-        />
+        <file-pond name="image" ref="pond" label-idle="Click to choose image, or drag here..." server="/upload"
+            accepted-file-types="image/*" @init="filePondInitialized" />
     </div>
 </template>
 
 <style scoped>
-
 </style>
